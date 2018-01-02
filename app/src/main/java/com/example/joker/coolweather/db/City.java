283c48455e4e2e15fb0ba927178545bc086a070d
jiangweihao <2928678686@qq.com>
2018-01-02 14:58:23 +0800

    V 2.0

--- app/src/main/java/com/example/joker/coolweather/db/City.java
+++ app/src/main/java/com/example/joker/coolweather/db/City.java
@@ -1,13 +1,51 @@
 package com.example.joker.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by Joker on 2018/1/2.
  */
 
-public class City {
+public class City extends DataSupport {
+
     private int id;
-    private  String cityName;
+
+    private String cityName;
+
     private int cityCode;
-    private int ProvieCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 
 }
