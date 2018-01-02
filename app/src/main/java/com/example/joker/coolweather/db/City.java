298a85ff6160b529ebc16b6d4a3f516692b319c6
jiangweihao <2928678686@qq.com>
2018-01-02 14:47:13 +0800

    V 1.0

---
+++ app/src/main/java/com/example/joker/coolweather/db/City.java
@@ -0,0 +1,13 @@
+package com.example.joker.coolweather.db;
+
+/**
+ * Created by Joker on 2018/1/2.
+ */
+
+public class City {
+    private int id;
+    private  String cityName;
+    private int cityCode;
+    private int ProvieCode;
+
+}
---
+++ app/src/main/java/com/example/joker/coolweather/db/County.java
@@ -0,0 +1,8 @@
+package com.example.joker.coolweather.db;
+
+/**
+ * Created by Joker on 2018/1/2.
+ */
+
+public class County  {
+}
